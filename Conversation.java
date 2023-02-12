a11831ad067f5598c763ead9d6d93e6ff668fad3
annaleethompson <acthompson@smith.edu>
2023-02-12 13:46:08 -0500

    Day2_1

--- Conversation.java
+++ Conversation.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
-import java.lang.*;
+import jave.util.ArrayList;
+//import java.lang.*;
 
 
-class Conversation {
+
+public class Conversation {
 
   public static void main(String[] arguments) {
     // You will start the conversation here.
     int rounds = 0;
     String response = ""; // or {}
     String[] split = response.split(" ");
-    StringBuffer transcript = new StringBuffer(" ");
+    //StringBuffer transcript = new StringBuffer(" ");
+    transcript.add(String)
 
     Scanner userInput = new Scanner(System.in);
     System.out.println("How many conversation rounds do you want?");
@@ -18,43 +21,43 @@ class Conversation {
     System.out.println("Hi there! What's on your mind?\n");
     transcript.append("Hi there! What's on your mind?\n");
 
-    for (int i = rounds) {
+    for (int i = 0; i<rounds, i++) {
       response = userInput.nextLine();
-        if split[0] = "I" || split[0] = "i" {
+        if (split[0] == "I" || split[0] == "i") {
           System.out.println("You", split[1:0]);
-          transcript.append("You", split[1:0], "\n");
+          transcript.add("You", split[1:0], "\n");
         }
         if split[-1] = "Me" || split[-1] = "me"{
           System.out.println(split[0:-1], "you");
-          transcript.append(split[0:-1], "you", "\n");
+          transcript.add(split[0:-1], "you", "\n");
         }
         if split[0] = "Am" || split[0] = "am" {
           System.out.println("Are", split[1:0]);
-          transcript.append("Are", split[1:0], "\n");
+          transcript.add("Are", split[1:0], "\n");
         }
         if split[0] = "You" || split[0] = "you" {
           System.out.println("I", split[1:0]);
-          transcript.append("I", split[1:0], "\n");
+          transcript.add("I", split[1:0], "\n");
         }
         if split[0] = "My" || split[0] = "my" {
           System.out.println("Your", split[1:0]);
-          transcript.append("Your", split[1:0], "\n");
+          transcript.add("Your", split[1:0], "\n");
         }
         if split[0] = "Your" || split[0] = "you" {
           System.out.println("My", split[1:0]);
-          transcript.append("My", split[1:0], "\n");
+          transcript.add("My", split[1:0], "\n");
         }
         if self.split[0] = "Are" || split[0] = "are" {
           System.out.println("Maybe");
-          transcript.append("Maybe", "\n")
+          transcript.add("Maybe", "\n")
         }
         else: {
           System.out.println("Hmm, interesting");
-          transcript.append("Hmm, interesting");
+          transcript.add("Hmm, interesting");
         }      
     }
   System.out.println("Goodbye!");
-      transcript.append("Goodbye!");
+      transcript.add("Goodbye!");
 
       userInput.close();
 }
